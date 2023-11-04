bc74448a4c91e7a18b7470bddf51b81518769826
Connor Burns <cpburns17@gmail.com>
2023-11-04 10:11:17 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {   
+    let messages = []
+    for (let i=0; i < names.length; i++) {
+        let mes = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        messages.push(mes)
+        debugger; 
+    }
+    return messages;
+} 
+writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise');
+
+function countDown() {
+    let i = 10;
+    while (i >= 0)
+    console.log(i--)
+}
+// countDown()
